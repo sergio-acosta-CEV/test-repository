6d6ffc84111a72a60b60d4d7ce5e4a530e644466
Sergio Acosta <sergio.acosta@hertasecurity.com>
2022-09-07 11:07:21 +0200

    Created XCode project

---
+++ ios-client/TestRepositoryIOSClient/TestRepositoryIOSClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestRepositoryIOSClient
+//
+//  Created by Sergio Acosta on 07/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
